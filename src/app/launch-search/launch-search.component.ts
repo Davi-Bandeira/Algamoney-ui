6d68a16949d6de353d963e6c92144aa061405d16
Davi-Bandeira <davibandeira2011@hotmail.com>
2021-05-25 14:45:03 -0300

    Aula 11.8. Desafio - usando pipes

--- src/app/launch-search/launch-search.component.ts
+++ src/app/launch-search/launch-search.component.ts
@@ -9,27 +9,27 @@ export class LaunchSearchComponent {
 
   public lancamentos = [
     {
-      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 6, 30) ,
       dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
     },
     {
-      tipo: 'DESPESA', descricao: 'Compra de Arroz', dataVencimento: '30/06/2017',
+      tipo: 'DESPESA', descricao: 'Compra de Arroz', dataVencimento: new Date(2017, 6, 30),
       dataPagamento: null, valor: 24.55, pessoa: 'Supermecado do João'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
+      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2012, 6, 10),
       dataPagamento: '09/06/2017', valor: 40055, pessoa: 'Atacado Brasil'
     },
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/06/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2018, 4, 13),
       dataPagamento: null, valor: 100, pessoa: 'Academia Top'
     },
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '20/06/2019',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2019, 6, 20),
       dataPagamento: null, valor: 90, pessoa: 'Academia Ritmos'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de Tablet', dataVencimento: '13/06/2020',
+      tipo: 'RECEITA', descricao: 'Venda de Tablet', dataVencimento: new Date(2020, 6, 13),
       dataPagamento: null, valor: 700, pessoa: 'Amigo de Gabriel'
     }
   ];
