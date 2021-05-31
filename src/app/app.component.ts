13a3d66f55558b6a9e2ccb2f8871284369d77a08
Davi-Bandeira <davibandeira2011@hotmail.com>
2021-05-31 09:46:44 -0300

    Aula 12.6. Two-way binding com ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
+class Cliente {
+  name: string;
+  email: string;
+  profissao: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +16,18 @@ export class AppComponent {
 
   profissoes = ['Desenvolvedor', 'Empresário', 'Outra'];
   profissao = 'Outra';
+  client = new Cliente();
 
   // tslint:disable-next-line:typedef
   salvar(form: NgForm) {
-    console.log(this.profissao);
-    console.log(form.value.profissao);
+    this.client.name = form.value.nome;
+    this.client.email = form.value.email;
+    this.client.profissao = form.value.profissao;
+
+    // console.log(this.profissao);
+    // console.log(form.value.profissao);
+    console.log(form.value);
+    console.log(this.client);
   }
 }
 
